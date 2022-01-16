c33b8c223c718a5622767b3d7adf2ab833c5cf8b
Wenderson Santos <guto15santos@gmail.com>
2022-01-16 17:03:24 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -7,6 +7,7 @@ import { BASE_URL } from "utils/requests";
 
 function Listing() {
     const [pageNumber, setPageNumber] = useState(0);
+
     const [page, setPage] = useState<MoviePage>({
         content: [],
         last: true,
@@ -18,27 +19,36 @@ function Listing() {
         numberOfElements: 0,
         empty: true
     });
+
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}`)
             .then(response => {
                 const data = response.data as MoviePage;
                 setPage(data);
             });
     }, [pageNumber]);
+
+    const handlePageChange = (newPageNumber : number) => {
+        setPageNumber(newPageNumber);
+    }
+
     return (
         <>
-            <Pagination />
+            <Pagination page={page} onChange={handlePageChange}/>
             <div className="container">
                 <div className="row">
-                    {page.content.map(movie => (
-                        <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                            <MovieCard movie={movie} />
-                        </div>
-                    )
+                    {page.content.map(movie =>    (
+                            <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                                <MovieCard movie = {movie} />
+                            </div>
+                        )
                     )}
+
                 </div>
+
             </div>
         </>
+
     );
 }
 
